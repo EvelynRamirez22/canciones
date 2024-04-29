f1e9813bcdb1d1f81157de88f9e954bb72e0e310
Evelyn Ramirez <eveeramirez92@gmail.com>
2024-04-29 11:59:22 -0300

    Empece a realizar el db pero hay errores.

--- spotify/__init__.py
+++ spotify/__init__.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
+with app.app_context():
+    from db import init_app
+    init_app(app)
+
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
 @app.route('/')
 def hello():

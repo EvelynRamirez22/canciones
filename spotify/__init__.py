6819029c952e4b6abc6b26245d6a3c487728c212
Evelyn Ramirez <eveeramirez92@gmail.com>
2024-05-06 12:05:43 -0300

    Avanzamos con la pagina de canciones

--- spotify/__init__.py
+++ spotify/__init__.py
@@ -1,17 +1,21 @@
-from flask import Flask, send_file
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
 with app.app_context():
-    from db import init_app
-    init_app(app)
-
-# ensure the instance folder exists
-try:
-    os.makedirs(app.instance_path)
-except OSError:
-    pass
+    from . import db
+    db.init_app(app)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/cantantes')
+def cantantes():
+    base_de_datos = db.get_db
+    consulta = """
+         SELECT Name FROM artists
+    """
+    resultado = base_de_datos.execute(consulta)
+    lista_de_resultados = resultado.fetchall()
+    return render_template("cantantes.html", cantantes=lista_de_resultados) 
--- spotify/db.py
+++ spotify/db.py
@@ -1,12 +1,19 @@
+import os
 import sqlite3
 
 import click
 from flask import current_app, g
 
-db_folder = current_app.instance.path
+db_folder = current_app.instance_path
 db_name = 'spotify.sqlite'
 db_file = os.path.join(db_folder, db_name)
 db_sql_file = 'discos.sql'
+
+
+def dict_factory(cursor, row):
+    """Arma un diccionario con los valores de la fila"""
+    fields =[column[0] for column in cursor.description]
+    return {key: value for key, value in zip(fields,row)}
 
 def get_db():
     if 'db' not in g:
@@ -26,9 +33,15 @@ def close_db(e=None):
         db.close()
 
 def init_db():
+    # ensure the instance folder exists
+    try:
+        os.makedirs(current_app.instance_path)
+    except OSError:
+        pass
+
     db = get_db()
 
-    with current_app.open_resource('discos.sql') as f:
+    with current_app.open_resource(db_sql_file) as f:
         db.executescript(f.read().decode('utf8'))
 
 

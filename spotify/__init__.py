f4da3ed3f56b06bc6354517e8011d89c855b7b50
Evelyn Ramirez <eveeramirez92@gmail.com>
2024-05-13 11:01:51 -0300

    hice la lista de cantantes y canciones

--- spotify/__init__.py
+++ spotify/__init__.py
@@ -12,10 +12,22 @@ def hello():
 
 @app.route('/cantantes')
 def cantantes():
-    base_de_datos = db.get_db
+    base_de_datos = db.get_db()
     consulta = """
          SELECT Name FROM artists
+         ORDER BY Name ASC
     """
     resultado = base_de_datos.execute(consulta)
     lista_de_resultados = resultado.fetchall()
     return render_template("cantantes.html", cantantes=lista_de_resultados) 
+
+@app.route('/canciones')
+def canciones():
+    base_de_datos = db.get_db()
+    consulta = """
+         SELECT name FROM tracks
+         ORDER BY name ASC
+    """
+    resultado = base_de_datos.execute(consulta)
+    lista_de_resultados = resultado.fetchall()
+    return render_template("canciones.html", canciones=lista_de_resultados) 
